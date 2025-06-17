56afb83a1510a44c87f27e279cd9dc10724ce033
dzakyputra45 <dzakypratama45@gmail.com>
2025-06-17 16:34:05 +0700

    first commit

---
+++ secret.js
@@ -0,0 +1,6 @@
+// You found the secret file, congrats!
+// Here's a little reward for the curious:
+
+// Flag: TI404{4r3p_M4d4ng_opo_r40n0_l4UK}
+
+// But maybe try looking a bit deeper next time...
